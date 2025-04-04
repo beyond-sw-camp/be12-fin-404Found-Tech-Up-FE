e791754b6b8c2702d98ee88c9ba83540b0ba5ff1
Xen-alpha <hky7548@naver.com>
2025-04-04 12:15:49 +0900

    [Feat] add coupon page

---
+++ techUpFrontend/components/coupon/coupon-area.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- breadcrumb area start -->
+  <section class="breadcrumb__area include-bg pt-95 pb-50">
+    <div class="container">
+      <div class="row">
+        <div class="col-xxl-12">
+          <div class="breadcrumb__content p-relative z-index-1">
+            <h3 class="breadcrumb__title">최고의 혜택을 받으세요!</h3>
+            <div class="breadcrumb__list">
+              <span><nuxt-link href="/">Home</nuxt-link></span>
+              <span>쿠폰</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- breadcrumb area end -->
+
+  <!-- coupon area start -->
+  <div class="tp-coupon-area pb-120">
+    <div class="container">
+      <div class="row">
+        <div v-for="item in coupon_data" :key="item.id" class="col-xl-6">
+          <coupon-item :coupon="item" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- coupon area end -->
+</template>
+
+<script setup>
+import coupon_data from '@/data/coupon-data';
+</script>
