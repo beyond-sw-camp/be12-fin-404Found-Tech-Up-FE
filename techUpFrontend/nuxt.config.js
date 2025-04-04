13a448fd79e8afbc35f777b4cc89e1e8abe8cbe3
Xen-alpha <hky7548@naver.com>
2025-04-04 09:11:49 +0900

    [Fix] fix pacakage.json to install sass and bootstrap

--- techUpFrontend/nuxt.config.js
+++ techUpFrontend/nuxt.config.js
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Tech Up",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js",
+        },
+      ],
+    }
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ["legacy-js-api"],
+        },
+      },
+    },
+  },
+  css: [
+    "@/assets/css/font-awesome-pro.css",
+    "@/assets/css/flaticon_shofy.css",
+    "@/assets/scss/main.scss",
+  ],
 })
