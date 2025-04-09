7f28a1b0f8846f610868e00fff06f7f6a5ecf2b4
Xen-alpha <hky7548@naver.com>
2025-04-09 12:52:09 +0900

    [Feat] add coupon list and admin alert pages

--- techUpFrontend/components/admin/admin-alert.vue
+++ techUpFrontend/components/admin/admin-alert.vue
@@ -1,5 +1,44 @@
 <template>
-
+  <div class="profile__ticket table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">알림 제목</th>
+          <th scope="col">알림 내용</th>
+          <th scope="col">상태</th>
+          <th scope="col">확인하기</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"> #2225</th>
+          <td data-info="title">GTX 1650 이벤트 시작</td>
+          <td data-info="status pending">확인 필요</td>
+          <td><a href="#" class="tp-logout-btn">내용 보기</a></td>
+        </tr>
+        <tr>
+          <th scope="row"> #2224</th>
+          <td data-info="title">HP PC 특별 할인 이벤트 시작</td>
+          <td data-info="status done">확인함</td>
+          <td><a href="#" class="tp-logout-btn">내용 보기</a></td>
+        </tr>
+        <tr>
+          <th scope="row"> #2223</th>
+          <td data-info="title">RTX 4070 Ti 쿠폰 발행</td>
+          <td data-info="status done">확인함</td>
+          <td><a href="#" class="tp-logout-btn">내용 보기</a></td>
+        </tr>
+        <tr>
+          <th scope="row"> #2222</th>
+          <td data-info="title">삼성 RAM 쿠폰 발행</td>
+          <td data-info="status done">확인함</td>
+          <td><a href="#" class="tp-logout-btn">내용 보기</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
 <script setup>
+
 </script>
