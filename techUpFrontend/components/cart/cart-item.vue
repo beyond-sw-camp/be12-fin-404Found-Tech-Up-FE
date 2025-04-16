192ae6f2008c3ad8a97d7ec419c6fb2efbf0552a
goodleaf96 <ky1234tho@gmail.com>
2025-04-16 12:11:16 +0900

    [feat] 장바구니 페이지 추가

---
+++ techUpFrontend/components/cart/cart-item.vue
@@ -0,0 +1,47 @@
+<template>
+  <tr>
+    <!-- img -->
+    <td class="tp-cart-img">
+      <nuxt-link :href="`/product-details/${item.idx}`" style="background-color: #F2F3F5;display: block;">
+        <img :src="item.img" alt=""/>
+      </nuxt-link>
+    </td>
+    <!-- title -->
+    <td class="tp-cart-title">
+      <nuxt-link :href="`/product-details/${item.idx}`">{{ item.name }}</nuxt-link>
+    </td>
+    <!-- price -->
+    <td class="tp-cart-price">
+      <span>{{ formatPrice(item.price) }}</span>
+    </td>
+    <!-- quantity -->
+    <td class="tp-cart-quantity">
+      <div class="tp-product-quantity mt-10 mb-10">
+        <span class="tp-cart-minus" @click="cartStore.quantityDecrement(item)">
+          <svg-minus />
+        </span>
+        <input class="tp-cart-input" type="text" v-model="item.orderQuantity" />
+        <span class="tp-cart-plus" @click="cartStore.addCartProduct(item)">
+          <svg-plus-sm />
+        </span>
+      </div>
+    </td>
+    <!-- action -->
+    <td class="tp-cart-action">
+      <button class="tp-cart-action-btn" @click="cartStore.removeCartProduct(item)">
+        <svg-remove />
+        <span>Remove</span>
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script setup>
+import { useCartStore } from "@/pinia/useCartStore";
+import { formatPrice } from "@/utils/index";
+
+const cartStore = useCartStore();
+const props = defineProps({
+  item: Object
+});
+</script>
