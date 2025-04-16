4ac1f8dfa2c40c563c6b81599526383df5c7b8ed
goodleaf96 <ky1234tho@gmail.com>
2025-04-16 11:33:01 +0900

    [feat] 위시리스트에서 할인율 적용된 가격 표시

--- techUpFrontend/components/wishlist/wishlist-item.vue
+++ techUpFrontend/components/wishlist/wishlist-item.vue
@@ -2,7 +2,8 @@
   <tr>
     <!-- img -->
     <td class="tp-cart-img">
-      <nuxt-link :href="`/product-details/${item.product.productIdx}`" style="background-color: #F2F3F5;display: block;">
+      <nuxt-link :href="`/product-details/${item.product.productIdx}`"
+        style="background-color: #F2F3F5;display: block;">
         <img :src="item.product.imageUrl" alt="image" />
       </nuxt-link>
     </td>
@@ -11,7 +12,17 @@
       <nuxt-link :href="`/product-details/${item.product.productIdx}`">{{ item.product.name }}</nuxt-link>
     </td>
     <!-- price -->
-    <td class="tp-cart-price"><span>{{ formatPrice(item.product.price) }}</span></td>
+    <td class="tp-cart-price">
+      <span v-if="item.product.discount && Number(item.product.discount) > 0">
+        <span class="tp-product-details-price old-price">{{ formatPrice(item.product.price, false) }}</span>
+        <span class="new-price">
+          {{ formatPrice(item.product.price - (item.product.price * Number(item.product.discount)) / 100) }}
+        </span>
+      </span>
+      <span v-else>
+        {{ formatPrice(item.product.price) }}
+      </span>
+    </td>
 
     <td class="tp-cart-add-to-cart">
       <button @click="cartStore.addCartProduct(item.product)" type="button" class="tp-btn tp-btn-2 tp-btn-blue">
