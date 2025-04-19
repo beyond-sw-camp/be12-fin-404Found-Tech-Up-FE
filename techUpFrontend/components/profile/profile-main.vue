d76d62947c8d28615f782058c01afc15b9ebb8cf
museongkim <mstar0228@gmail.com>
2025-04-19 14:40:04 +0900

    BTS-42 [Feat] 마이페이지(메인) - 사용자 닉네임, 디바이스 수 연결결

--- techUpFrontend/components/profile/profile-main.vue
+++ techUpFrontend/components/profile/profile-main.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { useUserStore } from '@/pinia/useUserStore'; // useUserStore import 추가
+import { useDeviceStore } from '../../pinia/useDeviceStore';
 import { useWishlistStore } from '@/pinia/useWishlistStore'; // 위시리스트 스토어 가져오기
 import { ref, onMounted } from 'vue';
 
+const deviceStore = useDeviceStore(); // 디바이스 스토어 초기화
 const wishlistStore = useWishlistStore(); // 위시리스트 스토어 초기화
 const userName = ref('test'); // 사용자 이름을 저장할 ref 변수
 const valueCount = ref({
@@ -49,7 +51,7 @@ const logout = async () => {
               </div>
             </div> -->
             <div class="profile__main-content">
-              <h4 class="profile__main-title">환영합니다, <span>{{ userName }}</span>님!</h4>
+              <h4 class="profile__main-title">환영합니다, <span>{{ userStore.userInfo.userNickname }}</span>님!</h4>
               <p>당신에게 <span>{{ valueCount.alarmsCount }}</span>개 알림이 있습니다</p>
             </div>
           </div>
@@ -68,7 +70,7 @@ const logout = async () => {
             <nuxt-link to="/wishlist" class="nav-link">
               <div class="profile__main-info-icon">
                 <span>
-                  <span class="profile-icon-count profile-download">{{ valueCount.devicesCount }}</span>
+                  <span class="profile-icon-count profile-download">{{ deviceStore.registerList.length }}</span>
                   <svg-download />
                 </span>
               </div>
