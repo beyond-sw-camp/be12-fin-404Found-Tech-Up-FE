da8f876e908c4b10022dd844595cc97209eaa626
Xenα <senouis@gmail.com>
2025-04-25 16:56:49 +0900

    Revert "[Fix] 메인페이지 카테고리 접근"

--- techUpFrontend/components/shop/sidebar/filter-categories.vue
+++ techUpFrontend/components/shop/sidebar/filter-categories.vue
@@ -3,13 +3,13 @@
     <ul>
       <li>
         <a @click.prevent="selectCategory('')" :class="{ active: !route.query.category }">
-          전체 카테고리
+          전체 카테고리 
           <!-- <span>{{ products.length }}</span> -->
         </a>
       </li>
       <li v-for="cat in categories" :key="cat.id">
         <a @click.prevent="selectCategory(cat.slug)" :class="{ active: route.query.category === cat.slug }">
-          {{ cat.parent }}
+          {{ cat.parent }} 
           <!-- <span>{{ cat.products.length }}</span> -->
         </a>
       </li>
@@ -26,18 +26,7 @@ const route = useRoute();
 const router = useRouter();
 const store = useProductFilterBackStore();
 
-onMounted(async () => {
-  console.log(route.query);
-  let category = route.query.category;
-
-  let [onSale, inStock] = route.query.status ? route.query.status.split(",") : [null, null];
-
-  let queryBody = {
-    category: category
-  }
-  if (route.query.category) await store.findProducts(queryBody);
-  else await store.fetchProducts();
-});
+onMounted(store.fetchProducts);
 
 const categories = computed(() => store.categories);
 const products = computed(() => store.products);
