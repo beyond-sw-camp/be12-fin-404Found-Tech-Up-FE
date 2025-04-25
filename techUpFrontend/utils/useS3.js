2e784073fd172e092072da09f37d86bfcf3e0a28
gunha0405 <dhrjsgk0405@gmail.com>
2025-04-25 14:50:17 +0900

    [Fix] S3 링크 수정

--- techUpFrontend/utils/useS3.js
+++ techUpFrontend/utils/useS3.js
@@ -1,6 +1,6 @@
 // utils/useS3.js
 export const buildS3Url = (key) => {
-    const baseUrl = 'https://ogh-spring-prac.s3.ap-northeast-2.amazonaws.com/';
+    const baseUrl = 'https://frontbucket-xenamaniac.s3.ap-northeast-2.amazonaws.com/';
     return `${baseUrl}${key}`;
   };
   
